63d3c30b57e59fe5f4d7efc67d497c51b4484864
Leonardo Mussato <leo.muss@gmail.com>
2021-03-29 01:27:26 +0200

    added mapAllResource, activateproduction, production to board

--- ProjectIngSW/src/main/java/Board.java
+++ ProjectIngSW/src/main/java/Board.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 public class Board {
+  private Map<ResourceType, Integer> chest;
   private Map<DevCardPosition, List<DevelopmentCard>> mapTray;
-  private Map<ResourceType, List<Resource>> chest;
   private Warehouse warehouse;
   private List<LeaderCard> leaderCards;
 
-  public Map<DevCardPosition, List<DevelopmentCard>> getMaptray() {
+  public Map<DevCardPosition, List<DevelopmentCard>> getMapTray() {
     return mapTray;
   }
 
-  public Map<ResourceType, List<Resource>> getChest() {
+  public Map<ResourceType, Integer> getChest() {
     return chest;
   }
 
@@ -20,5 +20,23 @@ public class Board {
 
   public List<LeaderCard> getLeaderCards() {
     return leaderCards;
+  }
+
+  public Map<ResourceType, Integer> mapAllResources() {
+    Map<ResourceType, Integer> currentResourcesMap = new HashMap<ResourceType, Integer>();
+    currentResourcesMap = warehouse.mapAllResources();
+    return currentResourcesMap;
+  }
+
+  public boolean activateProductionOnCard() {
+    return true;
+  }
+
+  public Map<DevCardPosition, DevelopmentCard> getCurrentProductionCards() { //prog funz?
+    Map<DevCardPosition, DevelopmentCard> productionCardsMap = new HashMap<DevCardPosition, DevelopmentCard>();
+    productionCardsMap.put(DevCardPosition.Left, this.mapTray.get(DevCardPosition.Left).get(this.mapTray.get(DevCardPosition.Left).size()-1));
+    productionCardsMap.put(DevCardPosition.Center, this.mapTray.get(DevCardPosition.Center).get(this.mapTray.get(DevCardPosition.Center).size()-1));
+    productionCardsMap.put(DevCardPosition.Right, this.mapTray.get(DevCardPosition.Right).get(this.mapTray.get(DevCardPosition.Right).size()-1));
+    return productionCardsMap;
   }
 }
