3096d7b21f87960093c1630cf968966ed0432c45
Leonardo Mussato <leo.muss@gmail.com>
2021-03-29 01:32:39 +0200

    added playTurn and all the actions that the player can do in his turn Co-authored-by: Flavio Rizzoglio <mightyflavieee@users.noreply.github.com> Co-authored-by: GianlucaRub <GianlucaRub@users.noreply.github.com>

--- ProjectIngSW/src/main/java/Market.java
+++ ProjectIngSW/src/main/java/Market.java
@@ -4,7 +4,6 @@ public class Market {
   // sarebbe da modificare inserendo la posizione delle biglie
   private List<Marble> tray;
   private Marble outsideMarble;
-
   public List<Marble> getTray() {
     return tray;
   }
@@ -12,8 +11,10 @@ public class Market {
     return outsideMarble;
   }
   //sarebbe da modificare in modo da poter inserire la biglia e poi shiftare quelle sulla stessa riga / colonna
-  public List<Resource> insertMarble(int position){
+  public List<Resource> insertMarble(int x, int y){
     return null;
   }
+
+ 
 
 }
--- ProjectIngSW/src/main/java/Player.java
+++ ProjectIngSW/src/main/java/Player.java
@@ -1,4 +1,5 @@
-
+import java.io.*;
+import java.util.*;
 
 public class Player {
   private Board board;
@@ -6,19 +7,135 @@ public class Player {
   private String nickname;
   private int victoryPoints;
 
-  public Board getBoard(){
+  public Player(String nickname) {
+    this.board = new Board();
+    this.isConnected = true; // Da gestire stato della connessione del player (con funzione apposita)
+    this.nickname = nickname;
+    this.victoryPoints = 0;
+  }
+
+  private int chooseAction() {
+    System.out.println(
+        "Choose which action you want to perform:\n1. Take Resources from the Market;\n2. Buy one Development Card;\n3. Activate the Production.");
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String chosenActionString = "";
+    try {
+      chosenActionString = reader.readLine();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    int chosenAction = Integer.parseInt(chosenActionString);
+    return chosenAction;
+  }
+
+  private Map<String, Integer> choosePosition() {
+    Map<String, Integer> mapOfPositions = new HashMap<String, Integer>();
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String chosenActionString = "";
+    System.out.println("X position? ");
+    while (true) {
+      try {
+        chosenActionString = reader.readLine();
+        mapOfPositions.put("x", Integer.parseInt(chosenActionString));
+        break;
+      } catch (Exception e) {
+        e.printStackTrace();
+        System.out.println("Unable to parse integer, insert a Integer!");
+      }
+    }
+    System.out.println("Y position? ");
+    while (true) {
+      try {
+        chosenActionString = reader.readLine();
+        mapOfPositions.put("y", Integer.parseInt(chosenActionString));
+        break;
+      } catch (Exception e) {
+        e.printStackTrace();
+        System.out.println("Unable to parse integer, insert a Integer!");
+      }
+    }
+    return mapOfPositions;
+  }
+
+  private boolean takeResourcesFromMarket(Market market) {
+    Map<String, Integer> chosenPosition = choosePosition();
+    List<Resource> acquiredResources = market.insertMarble(chosenPosition.get("x"), chosenPosition.get("y"));
+    Warehouse warehouse = this.board.getWarehouse();
+    warehouse.insertResources(acquiredResources);
+    return true;
+  }
+
+  private void showCurrentResources() {
+    Map<ResourceType, Integer> currentResources = this.board.mapAllResources();
+    System.out.println("Your total resources are:");
+    currentResources.entrySet().stream().forEach(singleMapObject -> {
+      System.out.println(singleMapObject.getKey() + ": " + singleMapObject.getValue());
+    });
+  }
+
+  private boolean buyDevelopmentCard(CardContainer cardContainer) {
+    while (true) {
+      showCurrentResources();
+      String idCardSelected = cardContainer.showAndSelectFromAvailableCards();
+      boolean hasBought = cardContainer.buy(idCardSelected, this.board);
+      if (!hasBought) {
+        System.out.println("Would you like to try to buy a different card?");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+          String chosenAction = reader.readLine();
+          if(chosenAction=="NO"){
+              return false;
+          }
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      } else {
+        return true;
+      }
+    }
+  }
+
+  private boolean activateProduction() {
+    boolean hasActivated = this.board.activateProductionOnCard();
+    
+    return true;
+  }
+
+  public Board getBoard() {
     return board;
   }
 
-  public boolean getStatus(){
+  public boolean getStatus() {
     return isConnected;
   }
 
-  public String getNickname(){
+  public String getNickname() {
     return nickname;
   }
 
-  public int getVictoryPoints(){
+  public int getVictoryPoints() {
     return victoryPoints;
+  }
+
+  public boolean playTurn(Market market, CardContainer cardContainer, ActionTokenContainer actionTokenContainer) {
+    boolean retry = false;
+    
+    while (!retry) {
+      int chosenAction = chooseAction();
+      switch (chosenAction) {
+      case 1:
+       retry = takeResourcesFromMarket(market);
+        break;
+      case 2:
+       retry = buyDevelopmentCard(cardContainer);
+        break;
+      case 3:
+       retry = activateProduction();
+        break;
+      default:
+        break;
+      }
+    }
+    return false;
   }
 }
