9326199ea0818364de412a12a1377986664c0c56
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2021-03-29 22:33:10 +0200

    Added PapalCouncilTile.numTile and implemented other methods

--- ProjectIngSW/src/main/java/PapalCouncilTile.java
+++ ProjectIngSW/src/main/java/PapalCouncilTile.java
@@ -1,11 +1,32 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class PapalCouncilTile implements ActivableTile{
     private List<Observer> observers;
+    private int numTile; // mi dice quale Council Tile sono
+
+    public PapalCouncilTile(int numTile) {
+        this.observers = new ArrayList<>();
+        this.numTile = numTile;
+    }
+
+    public int getNumTile() {
+        return numTile;
+    }
 
     @Override
-    public void activate() {}
-    public void attach(Observer observer){}
-    public void detach(Observer observer){}
-
+    public void activate(Player player) {
+        observers.forEach(Observer::update);
+    }
+    public void attach(PapalCouncilObserver observer){
+        this.observers.add(observer);
+        observer.setMyPapalCouncilTile(this);
+    }
+    //overload nel caso in cui abbia bisogno di attaccare un altro tipo di observer
+    public void attach(Observer observer){
+        this.observers.add(observer);
+    }
+    public void detach(Observer observer){
+        this.observers.remove(observer);
+    }
 }
