2337720ffd55457bbdc6138010c1dc236fef0c93
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2021-03-29 22:51:48 +0200

    added methods for the use of observer to VictoryPointsTile and implemented VictoryPointsTile.activate

--- ProjectIngSW/src/main/java/VictoryPointsTile.java
+++ ProjectIngSW/src/main/java/VictoryPointsTile.java
@@ -1,13 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class VictoryPointsTile implements ActivableTile{
+    private List<Observer> observers;
     private int victorypoints;
 
     public VictoryPointsTile(int victorypoints) {
+        this.observers = new ArrayList<>();
         this.victorypoints = victorypoints;
     }
 
-    @Override
-    public void activate(Player player) {
-    player.addVictoryPoints(victorypoints);
+    public int getVictorypoints() {
+        return victorypoints;
     }
-    //è necessario avere un riferimento al player perché altrimenti non posso aggiungergli i punti
+
+    @Override
+    public void activate() {
+        observers.forEach(Observer::update);
+    }
+    public void attach(VictoryPointsObserver observer){
+        this.observers.add(observer);
+        observer.setMyVictoryPointsTile(this);
+    }
+    //overload nel caso in cui abbia bisogno di attaccare un altro tipo di observer
+    public void attach(Observer observer){
+        this.observers.add(observer);
+    }
+    public void detach(Observer observer){
+        this.observers.remove(observer);
+    }
 }
