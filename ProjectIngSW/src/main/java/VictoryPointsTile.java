fd616edbeb5d4ec4ddcba7fef61d0642a5086752
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2021-03-29 22:28:29 +0200

    implemented VictoryPointsTile.activate

--- ProjectIngSW/src/main/java/VictoryPointsTile.java
+++ ProjectIngSW/src/main/java/VictoryPointsTile.java
@@ -1,8 +1,13 @@
 public class VictoryPointsTile implements ActivableTile{
     private int victorypoints;
 
-    @Override
-    public void activate() {
-
+    public VictoryPointsTile(int victorypoints) {
+        this.victorypoints = victorypoints;
     }
+
+    @Override
+    public void activate(Player player) {
+    player.addVictoryPoints(victorypoints);
+    }
+    //è necessario avere un riferimento al player perché altrimenti non posso aggiungergli i punti
 }
