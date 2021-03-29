a940eb8843b308876c11533de24976f13f06bf96
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2021-03-29 22:34:35 +0200

    Added PapalCouncilTile.numTile and implemented other methods

--- ProjectIngSW/src/main/java/PapalCouncilObserver.java
+++ ProjectIngSW/src/main/java/PapalCouncilObserver.java
@@ -1,8 +1,13 @@
 public class PapalCouncilObserver implements Observer{
     private Match match;
-
+    private PapalCouncilTile myPapalCouncilTile;
     @Override
     public void update() {
+        match.notifyFaithMapsForCouncil(myPapalCouncilTile.getNumTile());
 
+    }
+
+    public void setMyPapalCouncilTile(PapalCouncilTile myPapalCouncilTile) {
+        this.myPapalCouncilTile = myPapalCouncilTile;
     }
 }
