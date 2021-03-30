9c83ebfc4d8664191c9baf2ea29bae37a3b1a236
mightyflavieee <flaviorizzoglio@gmail.com>
2021-03-31 00:25:32 +0200

    Added addExtraDeposit(Resource resource) in Warehouse

--- ProjectIngSW/src/main/java/Warehouse.java
+++ ProjectIngSW/src/main/java/Warehouse.java
@@ -2,11 +2,14 @@ import java.util.*;
 
 public class Warehouse {
   private Map<ShelfFloor, List<Resource>> shelfs;
-  private Optional<Map<ExtraResourceType, List<Resource>>> extraDeposit;
+  private List<Resource> extraDeposit; // ATTENZIONE: per ora tolto l'Optional finché non capiamo come fare
   private List<Observer> observers;
-  
+
+  public Warehouse() {
+  }
+
   public Map<ResourceType, Integer> mapAllResources(){
-    Map<ResourceType, Integer> currentResourcesMap = new HashMap<ResourceType,Integer>();
+    Map<ResourceType, Integer> currentResourcesMap = new HashMap<>();
  
     return null;
   }
@@ -14,6 +17,7 @@ public class Warehouse {
   public Map<ShelfFloor, List<Resource>> getShelfs() {
     return shelfs;
   }
+
   public void insertResources(List<Resource> resourcesToBeInserted){}
 
   public void swapShelf(ShelfFloor swapper, ShelfFloor swappee){}
@@ -22,13 +26,24 @@ public class Warehouse {
     
   }
 
-  public Optional<Map<ExtraResourceType, List<Resource>>> getExtraDeposit(){
+  public List<Resource> getExtraDeposit(){
     return extraDeposit;
+  }
+
+  // adds resources to the extra deposit
+  public void addExtraDeposit(Resource resource) {
+    if (this.extraDeposit.size() < 2) {
+      this.extraDeposit.add(resource);
+    } else {
+      System.out.println("Extra Deposit is already full!");
+    }
   }
 
   public List<Observer> getObservers(){
     return observers;
   }
+
   public void attach(Observer observer){}
+
   public void detach(Observer observer){}
 }
