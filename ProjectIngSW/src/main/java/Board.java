3471670e39a6f791b3b6b2aa49f7530e0320d94f
mightyflavieee <flaviorizzoglio@gmail.com>
2021-03-30 22:42:47 +0200

    Added discardLeaderCard function

--- ProjectIngSW/src/main/java/Board.java
+++ ProjectIngSW/src/main/java/Board.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Board {
@@ -15,32 +17,49 @@ public class Board {
     return chest;
   }
 
-  public Warehouse getWarehouse() {
-    return warehouse;
-  }
+  public Warehouse getWarehouse() { return warehouse; }
 
   public List<LeaderCard> getLeaderCards() {
     return leaderCards;
   }
 
+  // verificare che questa funzione serva ---> c'è già in Warehouse
   public Map<ResourceType, Integer> mapAllResources() {
-    Map<ResourceType, Integer> currentResourcesMap = new HashMap<ResourceType, Integer>();
-    currentResourcesMap = warehouse.mapAllResources();
+    Map<ResourceType, Integer> currentResourcesMap = this.warehouse.mapAllResources();
     return currentResourcesMap;
   }
 
+  // da implementare per terza opzione di azioni del player
   public boolean activateProductionOnCard() {
     return true;
   }
 
   public Map<DevCardPosition, DevelopmentCard> getCurrentProductionCards() { //prog funz?
-    Map<DevCardPosition, DevelopmentCard> productionCardsMap = new HashMap<DevCardPosition, DevelopmentCard>();
+    Map<DevCardPosition, DevelopmentCard> productionCardsMap = new HashMap<>();
     productionCardsMap.put(DevCardPosition.Left, this.mapTray.get(DevCardPosition.Left).get(this.mapTray.get(DevCardPosition.Left).size()-1));
     productionCardsMap.put(DevCardPosition.Center, this.mapTray.get(DevCardPosition.Center).get(this.mapTray.get(DevCardPosition.Center).size()-1));
     productionCardsMap.put(DevCardPosition.Right, this.mapTray.get(DevCardPosition.Right).get(this.mapTray.get(DevCardPosition.Right).size()-1));
     return productionCardsMap;
   }
+
   public void moveForward(){
     faithMap.moveForward();
+  }
+
+  public List<LeaderCard> discardLeaderCard(List<LeaderCard> currentLeaderCards) {
+    System.out.println("Choose wich card to eliminate:");
+    for (LeaderCard card : currentLeaderCards) {
+      System.out.println((currentLeaderCards.indexOf(card) + 1) + ". ID:" + card.getId());
+    }
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    String chosenLeaderCard;
+    try {
+      chosenLeaderCard = reader.readLine();
+      currentLeaderCards.remove(Integer.parseInt(chosenLeaderCard));
+    } catch (Exception e) {
+      e.printStackTrace();
+      System.out.println("Cannot read commad!");
+    }
+    return currentLeaderCards;
   }
 }
