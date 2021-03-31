16b5d824abf814d5ae09a90bc759a66455a58739
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2021-03-31 16:26:00 +0200

    implemented MoveActionToken.Action and added MoveActionToken.attach and MoveActionToken.detach

--- ProjectIngSW/src/main/java/MoveActionToken.java
+++ ProjectIngSW/src/main/java/MoveActionToken.java
@@ -1,6 +1,15 @@
+import java.util.List;
+
 public class MoveActionToken implements ActionToken{
+    private List<Observer> observers;
     @Override
     public void Action() {
-
+        observers.forEach(Observer::update);
+    }
+    public void attach(Observer observer){
+        this.observers.add(observer);
+    }
+    public void detach(Observer observer){
+        this.observers.remove(observer);
     }
 }
