88f3ce78a494939234380c734ad5a8a928026579
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2021-04-06 20:26:41 +0200

    Implemented PapalFavourSlot constructor

--- ProjectIngSW/src/main/java/PapalFavourSlot.java
+++ ProjectIngSW/src/main/java/PapalFavourSlot.java
@@ -1,14 +1,19 @@
 public class PapalFavourSlot {
     private int victoryPoints;
     private PapalSlotStatus status;
+
+    public PapalFavourSlot(int victoryPoints) {
+        this.victoryPoints = victoryPoints;
+        this.status = PapalSlotStatus.Available;
+    }
+
     public int getVictoryPoints(){
         return  victoryPoints;
     }
-
     public PapalSlotStatus getStatus() {
         return status;
     }
-    public void UpdateStatus(PapalSlotStatus status){
+    public void updateStatus(PapalSlotStatus status){
         this.status = status;
     }
 }
