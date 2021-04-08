38176f900d3844455811dff0617a4587b1268329
mightyflavieee <flaviorizzoglio@gmail.com>
2021-04-08 22:03:43 +0200

    added test classes (JUnit5)

---
+++ src/test/java/BoardTest.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoardTest {
+
+    @Test
+    void getMapTray() {
+    }
+
+    @Test
+    void getChest() {
+    }
+
+    @Test
+    void getWarehouse() {
+    }
+
+    @Test
+    void getActivePerks() {
+    }
+
+    @Test
+    void getLeaderCards() {
+    }
+
+    @Test
+    void mapAllResources() {
+    }
+
+    @Test
+    void activateProductionOnCard() {
+    }
+
+    @Test
+    void getCurrentProductionCards() {
+    }
+
+    @Test
+    void moveForward() {
+    }
+
+    @Test
+    void discardLeaderCard() {
+    }
+
+    @Test
+    void papalCouncil() {
+    }
+}
---
+++ src/test/java/DiscardActionTokenTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiscardActionTokenTest {
+
+    @Test
+    void action() {
+    }
+
+    @Test
+    void attach() {
+    }
+
+    @Test
+    void detach() {
+    }
+}
