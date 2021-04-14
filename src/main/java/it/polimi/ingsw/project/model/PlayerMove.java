90d4fa22c1d84441f6544c822cfc5381c22a45f1
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2021-04-15 00:14:10 +0200

    Added PlayerMove.java

---
+++ src/main/java/it/polimi/ingsw/project/model/PlayerMove.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.project.model;
+
+import java.util.List;
+
+public class PlayerMove {
+    private final MoveType moveType;
+    private final Player player;
+    private final List<Resource> resources; //è generico, non sto specificando da dove prendo le risorse
+    private final View view;
+
+    public PlayerMove(Player player, MoveType moveType, List<Resource> resources, View view) {
+        this.player = player;
+        this.moveType = moveType;
+        this.resources = resources;
+        this.view = view;
+    }
+
+    public MoveType getMoveType() {
+        return moveType;
+    }
+
+    public List<Resource> getResources() {
+        return resources;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public View getView() {
+        return view;
+    }
+}
