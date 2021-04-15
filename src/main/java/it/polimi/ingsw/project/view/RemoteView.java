ff632e2c5e43141865e7e909b1515036de24338f
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2021-04-15 09:53:22 +0200

    Implemented first part of RemoteView.java

---
+++ src/main/java/it/polimi/ingsw/project/view/RemoteView.java
@@ -0,0 +1,69 @@
+package it.polimi.ingsw.project.view;
+
+import it.polimi.ingsw.project.model.Player;
+import it.polimi.ingsw.project.observer.Observer;
+import it.polimi.ingsw.project.server.ClientConnection;
+
+public class RemoteView extends View {
+
+    private class MessageReceiver implements Observer<String> {
+
+        @Override
+        public void update(String message) {
+            System.out.println("Received: " + message);
+            try{
+                //la stringa (oppure oggetto) che ricevo dal client deve essere in qualche modo convertita
+                //handleMove(movetipe, resources);
+            }catch(IllegalArgumentException e){
+                clientConnection.asyncSend("Error!");
+            }
+        }
+
+    }
+
+    private ClientConnection clientConnection;
+
+    public RemoteView(Player player, String opponent, ClientConnection c) {
+        super(player);
+        this.clientConnection = c;
+        c.addObserver(new MessageReceiver());
+        c.asyncSend("Your opponent is: " + opponent);
+
+    }
+
+    @Override
+    protected void showMessage(Object message) {
+        clientConnection.asyncSend(message);
+    }
+
+    @Override
+    public void update(MoveMessage message)
+    {
+        showMessage(message.getBoard());
+        String resultMsg = "";
+        boolean gameOver = message.getBoard().isGameOver(message.getPlayer().getMarker());
+        boolean draw = message.getBoard().isFull();
+        if (gameOver) {
+            if (message.getPlayer() == getPlayer()) {
+                resultMsg = gameMessage.winMessage + "\n";
+            } else {
+                resultMsg = gameMessage.loseMessage + "\n";
+            }
+        }
+        else {
+            if (draw) {
+                resultMsg = gameMessage.drawMessage + "\n";
+            }
+        }
+        if(message.getPlayer() == getPlayer()){
+            resultMsg += gameMessage.waitMessage;
+        }
+        else{
+            resultMsg += gameMessage.moveMessage;
+        }
+
+        showMessage(resultMsg);
+    }
+
+}
+
