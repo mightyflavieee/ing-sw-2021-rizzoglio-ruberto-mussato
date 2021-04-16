c0ce2b0706647f008ce384cc476ee6ccc4e1de5c
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2021-04-16 13:11:12 +0200

    Corrected Observer logic in LeaderCardObserver.java

--- src/main/java/it/polimi/ingsw/project/observer/custom/LeaderCardObserver.java
+++ src/main/java/it/polimi/ingsw/project/observer/custom/LeaderCardObserver.java
@@ -4,23 +4,17 @@ import it.polimi.ingsw.project.model.board.Board;
 import it.polimi.ingsw.project.model.board.card.leaderCard.perk.Perk;
 import it.polimi.ingsw.project.observer.Observer;
 
-public class LeaderCardObserver implements Observer {
+public class LeaderCardObserver implements Observer<Perk> {
     private Board board;
 
     public LeaderCardObserver(Board board) {
         this.board = board;
     }
 
-    @Override
-    public void update() {}
 
-    // overloading to update activePerks list in it.polimi.ingsw.project.model.board.Board
-    public void update(Perk perk) {
-        this.board.getActivePerks().add(perk);
-    }
     @Override
-    public void update(Object message) {
-        // TODO Auto-generated method stub
-        
+    public void update(Perk message) {
+        this.board.getActivePerks().add(message);
     }
+
 }
