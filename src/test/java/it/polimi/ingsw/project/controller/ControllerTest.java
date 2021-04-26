134c41f47a3ccedc2d8e3eae610cc800bf2ad452
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2021-04-26 10:29:59 +0200

    Added ControllerTest

---
+++ src/test/java/it/polimi/ingsw/project/controller/ControllerTest.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.project.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void update() {
+    }
+}
