97b1cf67e35f90da342bb5067211398eb4678f72
mightyflavieee <flaviorizzoglio@gmail.com>
2021-04-29 11:43:29 +0200

    added ProductionType enum for DevCardProductionMove

---
+++ src/main/java/it/polimi/ingsw/project/model/playermove/ProductionType.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.project.model.playermove;
+
+public enum ProductionType {
+    Board, DevCard, LeaderCard, BoardAndDevCard, BoardAndLeaderCard,
+    BoardAndDevCardAndLeaderCard, DevCardAndLeader
+}
