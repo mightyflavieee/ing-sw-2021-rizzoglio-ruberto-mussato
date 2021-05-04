bb893f1e69a6658456ad4b10e903ca903416356c
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2021-05-04 20:50:34 +0200

    Added Gson support and a test about it

--- src/main/java/it/polimi/ingsw/project/model/resource/Resource.java
+++ src/main/java/it/polimi/ingsw/project/model/resource/Resource.java
@@ -2,7 +2,7 @@ package it.polimi.ingsw.project.model.resource;
 
 import java.io.Serializable;
 
-public class Resource implements Serializable {
+public class Resource implements Serializable { //TODO serve davvero o basta resourceType?
     private ResourceType type;
 
     public Resource(ResourceType type) {
--- src/test/java/it/polimi/ingsw/project/model/board/card/leaderCard/LeaderCardTest.java
+++ src/test/java/it/polimi/ingsw/project/model/board/card/leaderCard/LeaderCardTest.java
@@ -1,6 +1,17 @@
 package it.polimi.ingsw.project.model.board.card.leaderCard;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+import it.polimi.ingsw.project.model.board.card.leaderCard.perk.PerkType;
+import it.polimi.ingsw.project.model.resource.ResourceType;
 import org.junit.jupiter.api.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LeaderCardTest {
 
@@ -22,5 +33,20 @@ class LeaderCardTest {
 
     @Test
     void testToString() {
+    }
+    @Test
+    void provaGson() throws FileNotFoundException {
+        Gson gson = new Gson();
+        JsonReader reader = new JsonReader(new FileReader("src/main/resources/Leadercards_Test_0.json"));
+        LeaderCard leaderCard = gson.fromJson(reader,LeaderCard.class);
+        assertEquals("1",leaderCard.getId());
+        assertEquals(ResourceType.Coin,leaderCard.getPerk().getResource().getType());
+        assertEquals(PerkType.Transmutation, leaderCard.getPerk().getType());
+//        assertEquals(1,leaderCard.getRequiredResources().get(ResourceType.Coin));
+//        assertEquals(1,leaderCard.getRequiredResources().get(ResourceType.Servant));
+//        assertEquals(1,leaderCard.getRequiredResources().get(ResourceType.Stone));
+        assertEquals(Status.Active,leaderCard.getStatus());
+
+
     }
 }
