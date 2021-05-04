27592c4ceff63e3a3f34ef205515cc1fa0e5f56f
mightyflavieee <flaviorizzoglio@gmail.com>
2021-05-04 18:46:04 +0200

    added test classes ProductionMoveTest, BuyDevCardMoveTest and ActivateLeaderCardMoveTest

---
+++ src/test/java/it/polimi/ingsw/project/model/playermove/ActivateLeaderCardMoveTest.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.project.model.playermove;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ActivateLeaderCardMoveTest {
+
+    @Test
+    void isFeasibleMove() {
+
+    }
+
+    @Test
+    void performMove() {
+
+    }
+}
