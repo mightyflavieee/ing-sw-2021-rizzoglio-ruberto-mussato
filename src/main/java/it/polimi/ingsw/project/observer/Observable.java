5781e2e46bef7a236a789781f5ca7376417bf965
LeonardoMussato <leo.muss@gmail.com>
2021-05-28 14:32:39 +0100

    changed to private for observers in observable

--- src/main/java/it/polimi/ingsw/project/observer/Observable.java
+++ src/main/java/it/polimi/ingsw/project/observer/Observable.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Observable<T> {
-    public final List<Observer<T>> observers = new ArrayList<>();
+    private final List<Observer<T>> observers = new ArrayList<>();
 
     public void addObserver(Observer<T> observer) {
         synchronized (observers) {
