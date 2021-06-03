6e276a9e5b2c50257e0648bfde7b3077a7f211f3
mightyflavieee <flaviorizzoglio@gmail.com>
2021-06-04 01:03:49 +0200

    updated BuyDevCardMoveHandler

--- src/main/java/it/polimi/ingsw/project/client/gui/BuyDevCardMoveHandler.java
+++ src/main/java/it/polimi/ingsw/project/client/gui/BuyDevCardMoveHandler.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.project.client.gui;
 
 import it.polimi.ingsw.project.model.board.DevCardPosition;
 import it.polimi.ingsw.project.model.board.card.developmentCard.DevelopmentCard;
+import it.polimi.ingsw.project.model.playermove.BuyDevCardMove;
+import it.polimi.ingsw.project.model.playermove.Move;
 import it.polimi.ingsw.project.model.resource.ResourceType;
 
 import java.util.HashMap;
@@ -25,4 +27,15 @@ public class BuyDevCardMoveHandler {
     public void setSelectResourcesHandler(SelectResourcesHandler selectResourcesHandler) { this.selectResourcesHandler = selectResourcesHandler; }
 
     public void setPosition(DevCardPosition position) { this.position = position; }
+
+    public Move getMove() {
+        return new BuyDevCardMove(this.developmentCard.getId(), this.position,
+                this.selectResourcesHandler.getResourcesFromWarehouse(), this.selectResourcesHandler.getResourcesFromChest());
+    }
+
+    public void reset() {
+        this.developmentCard = null;
+        this.selectResourcesHandler = null;
+        this.position = null;
+    }
 }
