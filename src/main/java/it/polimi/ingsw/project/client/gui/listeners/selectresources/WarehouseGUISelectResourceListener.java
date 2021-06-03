70c24a295ed6e9c590a5a196ba55b858c9d7c695
mightyflavieee <flaviorizzoglio@gmail.com>
2021-06-03 19:15:58 +0200

    added builder for ProductionMove

---
+++ src/main/java/it/polimi/ingsw/project/client/gui/listeners/selectresources/WarehouseGUISelectResourceListener.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.project.client.gui.listeners.selectresources;
+
+import it.polimi.ingsw.project.client.gui.InformationsGUI;
+import it.polimi.ingsw.project.client.gui.board.ExtraDepositsGUI;
+import it.polimi.ingsw.project.client.gui.board.WarehouseGUI;
+import it.polimi.ingsw.project.model.resource.ResourceType;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class WarehouseGUISelectResourceListener implements ActionListener {
+    private WarehouseGUI warehouseGUI;
+    private InformationsGUI informationsGUI;
+    private ResourceType resourceType;
+
+    public WarehouseGUISelectResourceListener(WarehouseGUI warehouseGUI,
+                                              InformationsGUI informationsGUI,
+                                              ResourceType resourceType) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
