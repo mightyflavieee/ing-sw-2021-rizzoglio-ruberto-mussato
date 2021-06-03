f8c51c8e437fa1a19d7d825a97b2da4d8a204df4
GianlucaRub <gianluca.ruberto@mail.polimi.it>
2021-06-03 11:02:45 +0200

    Added a test

--- src/test/java/it/polimi/ingsw/project/model/playermove/TakeMarketResourcesMoveTest.java
+++ src/test/java/it/polimi/ingsw/project/model/playermove/TakeMarketResourcesMoveTest.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.project.model.playermove;
 
 import it.polimi.ingsw.project.model.Match;
 import it.polimi.ingsw.project.model.Player;
+import it.polimi.ingsw.project.model.TurnPhase;
 import it.polimi.ingsw.project.model.board.ShelfFloor;
 import it.polimi.ingsw.project.model.board.Warehouse;
 import it.polimi.ingsw.project.model.market.Marble;
@@ -70,6 +71,61 @@ class TakeMarketResourcesMoveTest {
         Move move1Gian = new TakeMarketResourcesMove(warehouse1Gian, new ArrayList<>(), localMarket, false);
         assertTrue(move1Gian.isFeasibleMove(match));
 
+    }
+
+    @Test
+    void twoResourcesInShef2(){
+        Player gianluca = new Player("Gianluca");
+        Player flavio = new Player("Flavio");
+        Player leo = new Player("Leo");
+        List<Player> playerList = new ArrayList<>();
+        playerList.add(gianluca);
+        playerList.add(flavio);
+        playerList.add(leo);
+        Match match = new Match(playerList);
+
+        // removing the random beginning of the match
+        Marble[][] tray = new Marble[4][3];
+        List<Marble> trayList = new ArrayList<>();
+        trayList.add(new Marble(MarbleType.White));
+        trayList.add(new Marble(MarbleType.White));
+        trayList.add(new Marble(MarbleType.White));
+        trayList.add(new Marble(MarbleType.White));
+        trayList.add(new Marble(MarbleType.Blue));
+        trayList.add(new Marble(MarbleType.Blue));
+        trayList.add(new Marble(MarbleType.Grey));
+        trayList.add(new Marble(MarbleType.Grey));
+        trayList.add(new Marble(MarbleType.Yellow));
+        trayList.add(new Marble(MarbleType.Yellow));
+        trayList.add(new Marble(MarbleType.Purple));
+        trayList.add(new Marble(MarbleType.Purple));
+        trayList.add(new Marble(MarbleType.Red));
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 3; j++) {
+                tray[i][j] = trayList.remove(0);
+            }
+        }
+        Marble outsideMarble = trayList.remove(0);
+        match.getMarket().setOutsideMarble(outsideMarble);
+        assertEquals(outsideMarble.getType(), match.getMarket().getOutSideMarble().getType());
+        match.getMarket().setTray(tray);
+
+        // beginning of the match
+
+        // turn 1 gianluca
+        Market localMarket = new Market();
+        localMarket.setTray(tray);
+        localMarket.setOutsideMarble(outsideMarble);
+        List<Resource> resourceList1Gian = new ArrayList<>();
+        resourceList1Gian = localMarket.insertMarble(0, 1, null);
+        assertEquals(2,resourceList1Gian.size());
+        assertEquals(ResourceType.Shield,resourceList1Gian.get(0).getType());
+        assertEquals(ResourceType.Shield,resourceList1Gian.get(1).getType());
+        Warehouse warehouse1Gian = new Warehouse(null);
+        warehouse1Gian.insertInShelves(ShelfFloor.Second,resourceList1Gian);
+        Move move1Gian = new TakeMarketResourcesMove(warehouse1Gian, new ArrayList<>(), localMarket, false);
+        assertTrue(move1Gian.isFeasibleMove(match));
+        //todo da fixare
     }
 
 }
