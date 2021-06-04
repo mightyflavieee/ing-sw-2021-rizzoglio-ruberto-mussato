904685e9250bf7d074e57c1655e5c11a58711ac0
mightyflavieee <flaviorizzoglio@gmail.com>
2021-06-04 12:40:54 +0200

    added SelectCardForPurchaseListener

---
+++ src/main/java/it/polimi/ingsw/project/client/gui/listeners/selectcard/SelectCardForPurchaseListener.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.project.client.gui.listeners.selectcard;
+
+import it.polimi.ingsw.project.client.gui.InformationsGUI;
+import it.polimi.ingsw.project.model.board.card.developmentCard.DevelopmentCard;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SelectCardForPurchaseListener implements ActionListener {
+    private InformationsGUI informationsGUI;
+    private DevelopmentCard developmentCard;
+
+    public SelectCardForPurchaseListener(InformationsGUI informationsGUI, DevelopmentCard developmentCard) {
+        this.informationsGUI = informationsGUI;
+        this.developmentCard = developmentCard;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.informationsGUI.createSelectResourcesHandlerForPurchase(this.developmentCard);
+    }
+}
