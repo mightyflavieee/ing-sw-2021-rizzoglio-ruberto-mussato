299850fe36a8b666c957345fcbdbc56755050fe7
mightyflavieee <flaviorizzoglio@gmail.com>
2021-06-06 19:27:30 +0200

    added gui listeners for BuyDevCardMove

---
+++ src/main/java/it/polimi/ingsw/project/client/gui/listeners/informations/buydevcardmove/RightPositionPurchaseListener.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw.project.client.gui.listeners.informations.buydevcardmove;
+
+import it.polimi.ingsw.project.client.gui.GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RightPositionPurchaseListener implements ActionListener {
+    private GUI gui;
+
+    public RightPositionPurchaseListener(GUI gui) {
+        this.gui = gui;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
