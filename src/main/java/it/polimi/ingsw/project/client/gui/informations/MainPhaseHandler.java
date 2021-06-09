42feea0bd369df7f32db14e9bf24149280eb8ebb
mightyflavieee <flaviorizzoglio@gmail.com>
2021-06-10 01:55:23 +0200

    added Board required resources panel in MainPhaseHandler

--- src/main/java/it/polimi/ingsw/project/client/gui/informations/MainPhaseHandler.java
+++ src/main/java/it/polimi/ingsw/project/client/gui/informations/MainPhaseHandler.java
@@ -11,6 +11,7 @@ import it.polimi.ingsw.project.client.gui.listeners.informations.buydevcardmove.
 import it.polimi.ingsw.project.client.gui.listeners.informations.buydevcardmove.RightPositionPurchaseListener;
 import it.polimi.ingsw.project.client.gui.listeners.informations.productionmove.*;
 import it.polimi.ingsw.project.client.gui.listeners.informations.productionmove.boardproduction.*;
+import it.polimi.ingsw.project.client.gui.listeners.informations.productionmove.productiontypes.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -22,11 +23,13 @@ public class MainPhaseHandler extends JInternalFrame {
     private static final String PRODUCTIONPANEL = "Production Move";
     private static final String BUYDEVCARDMOVE = "Buy Dev Card Move";
     private static final String BOARDMANUFACTURINGPANEL = "Board Manufacturing";
+    private static final String BOARDREQUIREDRESOURCESPANEL = "Board Required Resources";
     private static final String ABORTMOVEPANEL = "Abort Move";
     private List<JButton> mainPhasesButtons;
     private List<JButton> buyDevCardButtons;
     private List<JButton> productionButtons;
     private List<JButton> boardManufacturingButtons;
+    private List<JButton> boardRequiredResourcesButtons;
     private JButton abortMoveButton;
     private JPanel mainPanel;
     private CardLayout mainPanelLayout;
@@ -34,6 +37,7 @@ public class MainPhaseHandler extends JInternalFrame {
     private JPanel buyDevCardPanel;
     private JPanel productionPanel;
     private JPanel boardManufacturingPanel;
+    private JPanel boardRequiredResourcesPanel;
     private JPanel abortMovePanel;
     private GUI gui;
 
@@ -50,6 +54,7 @@ public class MainPhaseHandler extends JInternalFrame {
         this.mainPanel.add(BUYDEVCARDMOVE, this.buyDevCardPanel);
         this.mainPanel.add(PRODUCTIONPANEL, this.productionPanel);
         this.mainPanel.add(BOARDMANUFACTURINGPANEL, this.boardManufacturingPanel);
+        this.mainPanel.add(BOARDREQUIREDRESOURCESPANEL, this.boardRequiredResourcesPanel);
         this.mainPanel.add(ABORTMOVEPANEL, this.abortMovePanel);
         this.mainPanelLayout.show(this.mainPanel, MAINPHASESPANEL);
         this.add(this.mainPanel);
@@ -61,16 +66,19 @@ public class MainPhaseHandler extends JInternalFrame {
         this.buyDevCardPanel = new JPanel();
         this.productionPanel = new JPanel();
         this.boardManufacturingPanel = new JPanel();
+        this.boardRequiredResourcesPanel = new JPanel();
         this.abortMovePanel = new JPanel();
         this.mainPhasesPanel.setVisible(true);
         this.buyDevCardPanel.setVisible(true);
         this.productionPanel.setVisible(true);
         this.boardManufacturingPanel.setVisible(true);
+        this.boardRequiredResourcesPanel.setVisible(true);
         this.abortMovePanel.setVisible(true);
         this.mainPhasesPanel.setLayout(new GridLayout(3, 1));
         this.buyDevCardPanel.setLayout(new GridLayout(4, 1));
         this.productionPanel.setLayout(new GridLayout(8, 1));
         this.boardManufacturingPanel.setLayout(new GridLayout(5, 1));
+        this.boardRequiredResourcesPanel.setLayout(new GridLayout(4, 2));
         this.abortMovePanel.setLayout(new BorderLayout());
         for (JButton button : this.mainPhasesButtons) {
             this.mainPhasesPanel.add(button);
@@ -84,6 +92,9 @@ public class MainPhaseHandler extends JInternalFrame {
         for (JButton button : this.boardManufacturingButtons) {
             this.boardManufacturingPanel.add(button);
         }
+        for (JButton button : this.boardRequiredResourcesButtons) {
+            this.boardRequiredResourcesPanel.add(button);
+        }
         this.abortMovePanel.add(this.abortMoveButton);
     }
 
@@ -92,6 +103,7 @@ public class MainPhaseHandler extends JInternalFrame {
         this.buyDevCardButtons = new ArrayList<>();
         this.productionButtons = new ArrayList<>();
         this.boardManufacturingButtons = new ArrayList<>();
+        this.boardRequiredResourcesButtons = new ArrayList<>();
         // creates main buttons
         JButton buttonMarket = new JButton("Take Resources From Market");
         JButton buttonPurchase = new JButton("Buy Development Card");
@@ -135,7 +147,7 @@ public class MainPhaseHandler extends JInternalFrame {
         buttonBoardAndDevCardProduction.addActionListener(new BoardDevCardProductionListener(this.gui));
         buttonBoardAndLeaderCardProduction.addActionListener(new BoardLeaderCardProductionListener(this.gui));
         buttonDevCardAndLeaderCardProduction.addActionListener(new DevLeaderCardsProductionListener(this.gui));
-        buttonBoardAndDevCardAndLeaderCardProduction.addActionListener(new BoardDevLeaderCardsProductionListener(this.gui));
+        buttonBoardAndDevCardAndLeaderCardProduction.addActionListener(new BoardDevLeaderCardsProdListener(this.gui));
         buttonGoBackProduction.addActionListener(new GoBackFromProductionListener(this.gui));
         // adds JButtons to attribute
         this.productionButtons.add(buttonBoardProduction);
@@ -166,10 +178,10 @@ public class MainPhaseHandler extends JInternalFrame {
                 new ImageIcon("src/main/resources/resourcetype/Stone.png")
                 .getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
         // adds listeners
-        buttonCoin.addActionListener(new CoinBoardProductionListener(this.gui));
-        buttonServant.addActionListener(new ServantBoardProductionListener(this.gui));
-        buttonShield.addActionListener(new ShieldBoardProductionListener(this.gui));
-        buttonStone.addActionListener(new StoneBoardProductionListener(this.gui));
+        buttonCoin.addActionListener(new CoinBoardProductionListener(this.gui, true));
+        buttonServant.addActionListener(new ServantBoardProductionListener(this.gui, true));
+        buttonShield.addActionListener(new ShieldBoardProductionListener(this.gui, true));
+        buttonStone.addActionListener(new StoneBoardProductionListener(this.gui, true));
         buttonGoBackBoardManufacturing.addActionListener(new GoBackFromBoardProduction(this.gui));
         // adds JButtons to attribute
         this.boardManufacturingButtons.add(buttonCoin);
@@ -177,6 +189,58 @@ public class MainPhaseHandler extends JInternalFrame {
         this.boardManufacturingButtons.add(buttonShield);
         this.boardManufacturingButtons.add(buttonStone);
         this.boardManufacturingButtons.add(buttonGoBackBoardManufacturing);
+        // creates Board required resources buttons
+        JButton buttonCoin1 = new JButton();
+        JButton buttonCoin2 = new JButton();
+        JButton buttonServant1 = new JButton();
+        JButton buttonServant2 = new JButton();
+        JButton buttonShield1 = new JButton();
+        JButton buttonShield2 = new JButton();
+        JButton buttonStone1 = new JButton();
+        JButton buttonStone2 = new JButton();
+        // sets resources icons
+        buttonCoin1.setIcon(new ImageIcon(
+                new ImageIcon("src/main/resources/resourcetype/Coin.png")
+                .getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+        buttonCoin2.setIcon(new ImageIcon(
+                new ImageIcon("src/main/resources/resourcetype/Coin.png")
+                .getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+        buttonServant1.setIcon(new ImageIcon(
+                new ImageIcon("src/main/resources/resourcetype/Servant.png")
+                .getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+        buttonServant2.setIcon(new ImageIcon(
+                new ImageIcon("src/main/resources/resourcetype/Servant.png")
+                .getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+        buttonShield1.setIcon(new ImageIcon(
+                new ImageIcon("src/main/resources/resourcetype/Shield.png")
+                .getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+        buttonShield2.setIcon(new ImageIcon(
+                new ImageIcon("src/main/resources/resourcetype/Shield.png")
+                .getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+        buttonStone1.setIcon(new ImageIcon(
+                new ImageIcon("src/main/resources/resourcetype/Stone.png")
+                .getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+        buttonStone2.setIcon(new ImageIcon(
+                new ImageIcon("src/main/resources/resourcetype/Stone.png")
+                .getImage().getScaledInstance(20, 20, Image.SCALE_SMOOTH)));
+        // adds listeners
+        buttonCoin1.addActionListener(new CoinBoardProductionListener(this.gui, false));
+        buttonCoin2.addActionListener(new CoinBoardProductionListener(this.gui, false));
+        buttonServant1.addActionListener(new ServantBoardProductionListener(this.gui, false));
+        buttonServant2.addActionListener(new ServantBoardProductionListener(this.gui, false));
+        buttonShield1.addActionListener(new ShieldBoardProductionListener(this.gui, false));
+        buttonShield2.addActionListener(new ShieldBoardProductionListener(this.gui, false));
+        buttonStone1.addActionListener(new StoneBoardProductionListener(this.gui, false));
+        buttonStone2.addActionListener(new StoneBoardProductionListener(this.gui, false));
+        // adds JButtons to attribute
+        this.boardRequiredResourcesButtons.add(buttonCoin1);
+        this.boardRequiredResourcesButtons.add(buttonCoin2);
+        this.boardRequiredResourcesButtons.add(buttonServant1);
+        this.boardRequiredResourcesButtons.add(buttonServant2);
+        this.boardRequiredResourcesButtons.add(buttonShield1);
+        this.boardRequiredResourcesButtons.add(buttonShield2);
+        this.boardRequiredResourcesButtons.add(buttonStone1);
+        this.boardRequiredResourcesButtons.add(buttonStone2);
         // creates Abort Move button
         this.abortMoveButton = new JButton("Abort Move");
         // adds listeners
@@ -196,6 +260,8 @@ public class MainPhaseHandler extends JInternalFrame {
     }
 
     public void goToBoardProductionButtons() { this.mainPanelLayout.show(this.mainPanel, BOARDMANUFACTURINGPANEL); }
+
+    public void goToBoardRequiredResourcesButtons() { this.mainPanelLayout.show(this.mainPanel, BOARDREQUIREDRESOURCESPANEL); }
 
     public void goToAbortMovePanel() { this.mainPanelLayout.show(this.mainPanel, ABORTMOVEPANEL); }
 }
